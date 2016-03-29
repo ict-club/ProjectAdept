a411a93665f9e7ce3377041c6cd1d0696b77bbfd
Martin Kuvandzhiev <martinkuvandzhiev@gmail.com>
2016-03-29 12:42:11 +0300

    Modified Train select screen

--- iOS/Adept_iOS/Adept_iOS/ExerciseViewController.m
+++ iOS/Adept_iOS/Adept_iOS/ExerciseViewController.m
@@ -33,23 +33,21 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
 #warning Incomplete implementation, return the number of sections
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
 #warning Incomplete implementation, return the number of rows
-    return 0;
+    return 4;
 }
 
-/*
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ExerciseTableCellIdentifier" forIndexPath:indexPath];
     
-    // Configure the cell...
     
     return cell;
 }
-*/
 
 /*
 // Override to support conditional editing of the table view.
