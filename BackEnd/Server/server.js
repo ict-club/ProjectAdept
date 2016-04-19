b9a6bc2729b1f7e3d04fa1bfc289e64e7e745c39
Borislav Filipov <boby.filipov@mail.bg>
2016-04-19 23:44:13 +0300

    zaqvki v0.0.0.0.1

--- BackEnd/Server/server.js
+++ BackEnd/Server/server.js
@@ -13,7 +13,20 @@ var pool = mysql.createPool({
     database: 'rapidde_adept',
     debug: false
 });
+function respond_to_options(res) {
+    // add needed headers
+    var headers = {};
+    headers["Access-Control-Allow-Origin"] = "*";
+    headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
+    headers["Access-Control-Allow-Credentials"] = true;
+    headers["Access-Control-Max-Age"] = '86400'; // 24 hours
+    headers["Access-Control-Allow-Headers"] = "X-Requested-With, Access-Control-Allow-Origin, X-HTTP-Method-Override, Content-Type, Authorization, Accept, parameters";
+    // respond to the request
+    res.writeHead(200, headers);
+    res.end();
+    return;
 
+}
 function execute_query(req, res, query, values) {
 
     pool.getConnection(function (err, connection) {
@@ -40,12 +53,15 @@ function execute_query(req, res, query, values) {
         connection.query(query, values, function (err, rows) {
             connection.release();
             if (!err) {
-                res.writeHead(200, { "Content-type": "application/json" });
+                var headers = {};
+                headers["Access-Control-Allow-Origin"] = "*";
+                headers["Content-type"] = "application/json";
+                res.writeHead(200, headers);
                 res.write(JSON.stringify(rows));
                 res.end();
             }
             else {
-                res.writeHead(406, { "error-code": err.code });
+                res.writeHead(400, { "error-code": err.code });
                 console.log(err);
                 res.write(err.code);
                 res.end();
@@ -54,7 +70,7 @@ function execute_query(req, res, query, values) {
         });
 
         connection.on('error', function (err) {
-            res.writeHead(406, { "error-code": err.code });
+            res.writeHead(400 , { "error-code": err.code });
             console.log(err);
             res.write(err.code);
             res.end();
@@ -65,6 +81,10 @@ function execute_query(req, res, query, values) {
 
 function users(request, response) {
     console.log("users");
+    if (request.method === 'OPTIONS') {
+        respond_to_options(response);
+        return;
+    }
     var sql = "SELECT id, Name, picture_small FROM userdata";
     execute_query(request, response, sql, [])
 }
@@ -72,9 +92,13 @@ function users(request, response) {
 function userData(request, response) {
     console.log("userdata");
     var parameters = request.headers["parameters"];
+    if (request.method === 'OPTIONS') {
+        respond_to_options(response);
+        return;
+    }
     if (parameters != null) {
         var values = [];
-        parameters_json = JSON.parse(parameters); 
+        parameters_json = JSON.parse(parameters);
         values.push(parameters_json[0].UserId);
         var sql = "select ud.id as UserId, ud.Name, ud.Age, udh.Height, ud.Title, ud.OverallCondition, udw.Weight, udm.avgForce, udwr.WristCirc, udc.CaloriesBalance, udhr.avgHeartRate as RestingHeartRate, ud.picture_big from userdata ud\
                     left join (select Weight, UserId from userdata_weight udw where timestamp = (select max(timestamp) from userdata_weight udw2 where udw2.UserId = udw.UserId)) udw on ud.id = udw.userId\
@@ -87,16 +111,161 @@ function userData(request, response) {
         execute_query(request, response, sql, values);
     }
     else {
-        response.writeHead(406, { "error-code": err.code });
-        console.log(err);
-        response.write(err.code);
+        response.writeHead(400, { "error-code": 400 });
+        response.write("400");
         response.end();
         return;
     }
 }
 
+function weightChart(request, response) {
+    console.log("weightChart");
+    var parameters = request.headers["parameters"];
+    if (request.method === 'OPTIONS') {
+        respond_to_options(response);
+        return;
+    }
+    if (parameters != null) {
+        var values = [];
+        parameters_json = JSON.parse(parameters);
+        values.push(parameters_json[0].UserId);
+        var fromDate = new Date(parameters_json[0].FromDate).getTime();
+        var toDate = new Date(parameters_json[0].ToDate).getTime();
+        fromDate = 1460937600000;
+        toDate = 1461283200001;
+        console.log(fromDate);
+        console.log(toDate);
+        var sql = "select avg(Weight) as Weight from userdata_weight where UserId = ? and Timestamp> ? and Timestamp <= ?";
+        var step = Math.floor((toDate - fromDate) / parameters_json[0].Points);
+        var addition = (toDate - fromDate) - parameters_json[0].Points * step;
+        console.log(addition);
+        for (var i = fromDate; i <= toDate - step; i += step) {
+            values[1] = i - (i == fromDate ? 1 : 0);
+            if (addition > 0)
+            {
+                i++;
+                addition--;
+            }
+            values[2] = i + step;
+            console.log(values.toString());
+            //execute_query(request, response, sql, values);
+        }
+
+        //execute_query(request, response, sql, values);
+    }
+    else {
+        response.writeHead(400, { "error-code": 400 });
+        console.log('400');
+        response.write(400);
+        response.end();
+        return;
+    } 
+}
+
+function select(request, response, tableName, userid)
+{
+    if (request.method === 'OPTIONS') {
+        respond_to_options(response);
+        return; 
+	}
+	if (userid == true) {
+		var parameters = request.headers["parameters"];
+		if (parameters != null) {
+			var values = [];
+			parameters_json = JSON.parse(parameters);
+			values.push(parameters_json[0].UserId);
+			var sql = "SELECT * FROM " + tableName + " where id = ?";
+			execute_query(request, response, sql, values);
+		}
+		else {
+			response.writeHead(400, { "error-code": 400 });
+			response.write("400");
+			response.end();
+			return;
+		}
+	}
+	else {
+		var sql = "SELECT * FROM " + tableName;
+		execute_query(request, response, sql, []);
+	}
+}
+
+function insert(request, response, tableName) {
+	if (request.method === 'OPTIONS') {
+		respond_to_options(response);
+		return;
+	}
+	if (request.method == 'POST') {
+		var body = [];
+		request.on('error', function (err) {
+			console.error(err);
+		}).on('data', function (chunk) {
+			body.push(chunk);
+		}).on('end', function () {
+			if (body != []) {
+				body = Buffer.concat(body).toString();
+				var values = [];
+				var columns = "";
+				var valuesstr = "";
+				var first = true;
+				body_json = JSON.parse(body);
+				var obj = body_json[0];
+				for (var key in obj) {
+					var attributeValue = obj[key];
+					if (first == true) {
+						first = false;
+					}
+					else {
+						columns += ", ";
+						valuesstr += ", ";
+					}
+					key = key.replace("'", "");
+					columns += key;
+					//if (typeof attributeValue === 'string') {
+					//	attributeValue = "'" + attributeValue + "'";
+					//}
+					values.push(attributeValue);
+					valuesstr += '?';
+				}
+				
+				
+				var sql = "INSERT INTO "+tableName+"(" + columns + ") VALUES (" + valuesstr + ")";
+				execute_query(request, response, sql, values);
+			}
+			else {
+				response.writeHead(400, { "error-code": 400 });
+				response.write("400");
+				response.end();
+				return;
+			}
+		});
+	}
+}
+
+function userData_select(request, response) {
+	console.log("userData_select");
+	select(request, response, "userdata", true);
+}
+function userData_insert(request, response) {
+	console.log("userData_insert");
+	insert(request, response, "userdata");
+}
+
+function userData_calories_select(request, response) {
+	console.log("userData_calories_select");
+	select(request, response, "userdata_calories", true);
+}
+function userData_calories_insert(request, response) {
+	console.log("userData_calories_insert");
+	insert(request, response, "userdata_calories");
+}
 app.use('/users', users);
 app.use('/userData', userData);
+app.use('/weightChart', weightChart);
+app.use('/userData_select', userData_select);
+app.use('/userData_insert', userData_insert);
+app.use('/userData_calories_select', userData_calories_select);
+app.use('/userData_calories_insert', userData_calories_insert);
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 process.setMaxListeners(0);
