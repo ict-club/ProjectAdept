70ed4853d81dd81ccb4398342058477e02e8899b
Martin Kuvandzhiev <martinkuvandzhiev@gmail.com>
2016-04-19 21:58:36 +0300

    Added some files

---
+++ iOS/Adept_iOS/Adept_iOS/PickerTableCell.h
@@ -0,0 +1,15 @@
+//
+//  pickerTableCell.h
+//  Adept_iOS
+//
+//  Created by Martin Kuvandzhiev on 4/19/16.
+//  Copyright © 2016 Martin Kuvandzhiev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PickerTableCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIPickerView *pickerView;
+
+@end
---
+++ iOS/Adept_iOS/Adept_iOS/PickerTableCell.m
@@ -0,0 +1,13 @@
+//
+//  pickerTableCell.m
+//  Adept_iOS
+//
+//  Created by Martin Kuvandzhiev on 4/19/16.
+//  Copyright © 2016 Martin Kuvandzhiev. All rights reserved.
+//
+
+#import "PickerTableCell.h"
+
+@implementation PickerTableCell
+
+@end
