b6f73055be9f11d09e20bdfc59deb193949e1d87
PETAR LAZAROV <primorack@abv.bg>
2016-04-22 19:56:10 +0300

    - added log in food

--- iOS/Adept_iOS/Adept_iOS/AddFoodViewController.h
+++ iOS/Adept_iOS/Adept_iOS/AddFoodViewController.h
@@ -9,5 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @interface AddFoodViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIButton *buttonAddFood;
+@property (weak, nonatomic) IBOutlet UIView *tableViewDataFromFood;
 
 @end
--- iOS/Adept_iOS/Adept_iOS/AddFoodViewController.m
+++ iOS/Adept_iOS/Adept_iOS/AddFoodViewController.m
@@ -12,7 +12,12 @@
 
 - (void) viewDidLoad
 {
-    
+    self.buttonAddFood.layer.cornerRadius = 6.5;
+    self.buttonAddFood.layer.masksToBounds = YES;
+    self.buttonAddFood.layer.borderWidth = 0.1;
+    self.tableViewDataFromFood.layer.cornerRadius = 6.5;
+    self.tableViewDataFromFood.layer.masksToBounds = YES;
+    self.tableViewDataFromFood.layer.borderWidth = 1.0;
 }
 
 - (void) viewDidAppear:(BOOL)animated
